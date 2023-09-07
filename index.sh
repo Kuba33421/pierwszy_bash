dd3308fb0b6b4036f9f4963db4e6f2a1a7681a26
Kuba33421 <144320743+Kuba33421@users.noreply.github.com>
2023-09-07 12:31:46 +0200

    index.sh

--- index.sh
+++ index.sh
@@ -35,9 +35,9 @@ syfruj(){
     local tekst"$1"
     local zaszyfrowany_tekst""
     
-    for litera in $(echo "$tekst") | tr '[:lower]' '[upper]' #zmienia male litery na duze 
+    for litera in $(echo "$tekst") | tr '[:lower]' '[:upper]' #zmienia male litery na duze 
     do 
-    if [[ "$litera =~ [A-Z]"]]; then
+    if [[ "$litera =~ "[A-Z]"]]; then
     for i in {0..4}; do #przedzial liczb z siatki 
         for j in {0..4}; do 
             if [[ "${siatka[i]:$j:1}" == "$litera"]]; then
@@ -46,7 +46,7 @@ syfruj(){
         done
     done
     else 
-        aszyfrowany_tekst+="$litera"
+        zaszyfrowany_tekst+="$litera"
     fi
     done
 
@@ -66,4 +66,41 @@ deszyfruj(){
     done
 
     echo "$odszyfrowany_tekst"
-}
+} else
+
+
+stworz_siatke
+
+#sprawadzanie argument
+if [ $# -lt 2]; then
+    echo "Uzycie: $0 -in <plik_wejsciowy> [-out <plik_wyjsciowy>]"
+    exit 1
+fi
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in 
+        -in)
+        plik_wejsciowy="$2"
+        shift
+        shift
+        ;;
+    -out)
+        plik_wyjsciowy="$2"
+        shift
+        shift
+        ;;
+    *)
+        shift
+        ;;
+    esac
+done
+
+if [ -z "$plik_wejsciowy"]; then 
+    zaszyfrowana_wiadomosc=$(szyfruj "$(cat "$plik_wejsciowy")")
+    echo "Zaszyfrowana wiadomosc:"
+    echo "$zaszyfrowana_wiadomosc"
+else
+
+    zaszyfrowana_wiadomosc=$(szefruj "$(cat "$plik_wejsciowy")")
+    echo "zaszyfrowana_wiadomosc" > "$plik_wejsciowy"
+    echo "Zaszyfrowana_wiadomosc zapisana do pliku $plik_wyjsciowy"
